41a0c317e285780b5f20a4129f327ebd9b805afe
oluochelvis <oduorelvis247@gmail.com>
2022-12-21 13:00:49 +0300

    9-print_comb.c

---
+++ 0x00-hello_world/0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main -  program that prints all possible combinations of single-digit
+ * Nos must be separated by ,followed by a space
+ * Nos should be printed in ascending o
+ * only use the putchar function
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int n = 48;
+	int com = 44;
+	while (n <= 57)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(com);
+			putchar(32);
+		}
+		n += 1;
+	}
+	putchar('\n');
+	return (0);
+}
